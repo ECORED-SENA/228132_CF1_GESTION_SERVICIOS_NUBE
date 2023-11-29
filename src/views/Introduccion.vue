cf7f0e34b387a5c74e1f6bbb6910b1a6b035daac
Andrea Botello <andrea.botellor@misena.edu.co>
2023-11-29 10:06:35 -0500

    Video, Accesibilidad y .zip

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -18,8 +18,9 @@
     
     figure(data-aos="fade-down")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption Video.
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/AGaxodjQeSU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      figcaption Video. Conceptos básicos de redes y networking
+
 
 
 
